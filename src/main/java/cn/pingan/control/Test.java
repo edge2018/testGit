9630e2611ca6ad0e510478387898e8c0777df18b
jianyou05 <jianyou05@jianyou05-PC>
2019-04-25 13:49:13 +0800

    update

--- src/main/java/cn/pingan/control/Test.java
+++ src/main/java/cn/pingan/control/Test.java
@@ -13,6 +13,20 @@ public class Test {
 		arr.add(1);
 		arr.add(1);
 		System.out.println(arr);
+		
+		int[] a = {134,33,31,48,25,43,32};
+		for (int i = 0; i < a.length-1; i++) {
+			for(int j=0;j<a.length-i-1;j++) {
+				if(a[j]<a[j+1]) {
+					int temp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
 	}
 	
 }
