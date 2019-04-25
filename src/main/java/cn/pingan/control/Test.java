903f4a2328524ae5b1b90a254b1b418c2c0f901a
jianyou05 <jianyou05@jianyou05-PC>
2019-04-25 13:40:20 +0800

    update

--- src/main/java/cn/pingan/control/Test.java
+++ src/main/java/cn/pingan/control/Test.java
@@ -1,26 +1,18 @@
 package cn.pingan.control;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("==================");
-		记得看风景附加费
-                ============、
-        这不是一个Java项目
-		System.out.println("-----����git");
-		
-		System.out.println("----");
-		int a = 10;
-		int b = 20;
-		int c= a+b;
-		System.out.println(c);
-	}
-	public void add() {
-		System.out.println("��������Ͳ���������");
+		ArrayList<Object> arr = new ArrayList<Object>();
+		arr.add(1);
+		arr.add(1);
+		arr.add(1);
+		arr.add(1);
+		arr.add(1);
+		System.out.println(arr);
 	}
 	
-	public String islogin(String loginname,String loginpwd) {
-		return "��¼������";
-	}
 }
